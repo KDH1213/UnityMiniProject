3a0ece9f17b6adfcdc1e5b11d0c2d805aa6f0ed6
Kim.Dong.Hyun <ehdgus0710@naver.com>
2025-01-27 14:26:58 +0900

    상호작용 UI 레이어 추가

--- Assets/Scripts/Util/LayerMask.cs
+++ Assets/Scripts/Util/LayerMask.cs
@@ -9,6 +9,8 @@ public static class GetLayerMasks
 {
     public static readonly int Ground = 1 << GetLayer.Ground;
     public static readonly int Enemy = 1 << GetLayer.Enemy;
+    public static readonly int UI = 1 << GetLayer.UI;
+    public static readonly int InteractionUI = 1 << GetLayer.InteractionUI;
 }
 
 
@@ -19,4 +21,6 @@ public static class GetLayer
 {
     public static readonly int Ground = UnityEngine.LayerMask.NameToLayer("Ground");
     public static readonly int Enemy = UnityEngine.LayerMask.NameToLayer("Enemy");
+    public static readonly int UI = UnityEngine.LayerMask.NameToLayer("UI");
+    public static readonly int InteractionUI = UnityEngine.LayerMask.NameToLayer("InteractionUI");
 }
