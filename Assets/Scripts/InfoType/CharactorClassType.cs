ed4433f5fc94172acc94783e004947cb61139d3c
Kim.Dong.Hyun <ehdgus0710@naver.com>
2025-01-29 17:18:42 +0900

    캐릭터 클래스 플레그 추가

--- Assets/Scripts/InfoType/CharactorClassType.cs
+++ Assets/Scripts/InfoType/CharactorClassType.cs
@@ -1,8 +1,15 @@
-﻿
-[System.Serializable]
+﻿[System.Serializable]
 public enum CharactorClassType
 {
     N,
     A,
     S,
+}
+[System.Serializable]
+[System.Flags]
+public enum CharactorClassTypeMask
+{
+    N = 1 << CharactorClassType.N,
+    A = 1 << CharactorClassType.A,
+    S = 1 << CharactorClassType.S,
 }
