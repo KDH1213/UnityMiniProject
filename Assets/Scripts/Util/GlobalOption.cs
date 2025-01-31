3b49625654566bf98973635193e2aad5d53f77ee
김동현 <ehdgus0710@naver.com>
2025-01-31 14:33:49 +0900

    일시 정지 버그 수정

--- Assets/Scripts/Util/GlobalOption.cs
+++ Assets/Scripts/Util/GlobalOption.cs
@@ -6,21 +6,34 @@ public class GlobalOption : MonoBehaviour
 {
     [SerializeField]
     private int targetFrameRate;
+
+
+    private GameModeType currentMode = GameModeType.GamePlay;   
+
     public static float prevTimeScale;
 
     private void Awake()
     {
         Application.targetFrameRate = targetFrameRate;
+        prevTimeScale = Time.timeScale;
     }
 
     public void OnPause()
     {
+        if (currentMode == GameModeType.GamePause)
+        {
+            Time.timeScale = 0f;
+            return;
+        }
+
+        currentMode= GameModeType.GamePause;
         prevTimeScale = Time.timeScale;
         Time.timeScale = 0f;
     }
 
     public void OnRestart()
     {
+        currentMode= GameModeType.GamePlay;
         Time.timeScale = prevTimeScale;
     }
 
