07794eb13c50f24d37384bfdc182a2d2a64c3580
김동현 <ehdgus0710@naver.com>
2025-02-03 15:43:42 +0900

    캐릭터 애니메이션 유틸 추가

---
+++ Assets/Scripts/Contents/Util/CharactorAnimationUtil.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace DHUtil
+{
+    class CharactorAnimationUtil
+    {
+        public static readonly int hashIsMove = Animator.StringToHash("1_Move");
+        public static readonly int hashIsAttack = Animator.StringToHash("2_Attack");
+        public static readonly int hashIsDamaged = Animator.StringToHash("3_Damaged");
+        public static readonly int hashIsDeath = Animator.StringToHash("4_Death");
+        public static readonly int hashIsDebuff = Animator.StringToHash("5_Debuff");
+        public static readonly int hashIsOther = Animator.StringToHash("5_Other");
+        public static readonly int hashIsIsDeath = Animator.StringToHash("isDeath");
+        // public static readonly int hashIsAttack = Animator.StringToHash("isDeath");
+    }
+}
