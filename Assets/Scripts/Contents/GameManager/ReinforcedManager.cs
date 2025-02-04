3e58dff98b2471dd386e32bb82f3766e4e5bd123
김동현 <ehdgus0710@naver.com>
2025-02-04 16:25:37 +0900

    강화 매니저, 테이블 추가 및 ui 수정

---
+++ Assets/Scripts/Contents/GameManager/ReinforcedManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReinforcedManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameController gameController;
+    [SerializeField]
+    private InGameUiController inGameUiController;
+
+    private int currentReinforcedLevelTypeN = 0;
+    private int currentReinforcedLevelTypeA = 0;
+    private int currentReinforcedLevelTypeS = 0;
+    private int currentReinforcedLevelTypeCall = 0;
+
+
+    // private ReinforcedData reinforcedData;
+}
---
+++ Assets/Scripts/Util/DataTable/ReinforcedDataTable.cs
@@ -0,0 +1,58 @@
+﻿using CsvHelper;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public class ReinforcedData
+{
+    public int Id { get; set; }
+
+    public CharactorClassType CharactorClassType { get; set; }
+    public CurrencyType CurrencyType { get; set; }
+
+    public string Value { get; set; }
+    public string DamagePercent { get; set; }
+
+    public List<int> valueList = new List<int>();
+    public List<int> damagePercentList = new List<int>();
+}
+
+public class ReinforcedDataTable : DataTable
+{
+    private Dictionary<CharactorClassType, ReinforcedData> reinforcedDictionoary = new Dictionary<CharactorClassType, ReinforcedData>();
+    public override void Load(string filename)
+    {
+        var path = string.Format(FormatPath, filename);
+
+        var textAsset = Resources.Load<TextAsset>(path);
+        var reinforcedList = LoadCSV<ReinforcedData>(textAsset.text);
+
+        foreach (var item in reinforcedList)
+        {
+            if(!reinforcedDictionoary.ContainsKey(item.CharactorClassType))
+            {
+                item.valueList = item.Value.Split('_').Select(p => int.Parse(p)).ToList();
+                item.damagePercentList = item.DamagePercent.Split('_').Select(p => int.Parse(p)).ToList();
+                reinforcedDictionoary.Add(item.CharactorClassType, item);
+            }
+            else
+            {
+                Debug.LogError($"{item.Id} None");
+            }
+        }
+
+        reinforcedDictionoary.Clear();
+    }
+
+    public ReinforcedData GetKeyData(CharactorClassType key)
+    {
+        if (!reinforcedDictionoary.ContainsKey(key))
+        {
+            Debug.LogError($"{key} None");
+            return default;
+        }
+
+        return reinforcedDictionoary[key];
+    }
+}
