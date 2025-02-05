d1db613d6417cb753389651415f858cd388ef70c
Kim.Dong.Hyun <ehdgus0710@naver.com>
2025-02-06 00:25:23 +0900

    보석 뽑기 프로토 타입 수정 중

--- Assets/Scripts/Contents/UI/UIDrawJewelRoulette.cs
+++ Assets/Scripts/Contents/UI/UIDrawJewelRoulette.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.GraphicsBuffer;
 
 public class UIDrawJewelRoulette : MonoBehaviour
 {
@@ -15,9 +16,19 @@ public class UIDrawJewelRoulette : MonoBehaviour
     private float defalutRotationTime = 0.5f;
 
     [SerializeField]
-    private float rotationSpeed = 20f;
+    private float rotationSpeed = 50f;
 
     Coroutine coroutine;
+    // Quaternion target;
+    // Quaternion startRotation;
+
+
+    float currentTime = 0f;
+    bool isRotation = false;
+
+    bool isStart = false;
+    bool isEnd = false;
+
     private IEnumerator CoRatation()
     {
         float currentTime = 0f;
@@ -29,9 +40,21 @@ public class UIDrawJewelRoulette : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
 
-        var target = Random.Range(0f, 360f);
+        var target = Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f));
+        var startRotation = roulette.transform.rotation;
 
-        roulette.transform.rotation = Quaternion.Euler(Vector3.forward * target);
+        if(startRotation.z < 0f)
+        {
+            startRotation *= Quaternion.Euler(Vector3.forward * 360f);
+        }
+
+        currentTime = 0f;
+        while (currentTime < 0.1f)
+        {
+            currentTime += Time.deltaTime;
+            roulette.transform.rotation = Quaternion.Lerp(startRotation, target, currentTime / 0.1f);
+            yield return new WaitForEndOfFrame();
+        }
         coroutine = null;
     }
 
@@ -41,5 +64,44 @@ public class UIDrawJewelRoulette : MonoBehaviour
             coroutine = StartCoroutine(CoRatation());
         else
             return;
+
+        //isRotation = true;
+        //isStart = true;
     }
+
+    //private void Update()
+    //{
+    //    if (isRotation)
+    //    {
+
+    //        if(isStart)
+    //        {
+    //            currentTime += Time.deltaTime;
+    //            roulette.transform.rotation *= Quaternion.Euler(Vector3.forward * rotationSpeed);
+    //            if (currentTime > defalutRotationTime)
+    //            {
+    //                isStart = false;
+    //                target = Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f));
+    //                startRotation = roulette.transform.rotation;
+    //            }
+    //        }
+    //        else
+    //        {
+    //            if (startRotation.z < 0f)
+    //            {
+    //                startRotation *= Quaternion.Euler(Vector3.forward * 360f);
+    //            }
+
+    //            currentTime = 0f;
+
+    //            currentTime += Time.deltaTime;
+    //            roulette.transform.rotation = Quaternion.Lerp(startRotation, target, currentTime / 0.1f);
+    //            if (currentTime > 0.1f)
+    //            {
+    //                isRotation = false;
+    //            }
+
+    //        }
+    //    }
+    //}
 }
