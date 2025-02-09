d920f17eff775860c630794e66e198fa1213b28d
Kim.Dong.Hyun <ehdgus0710@naver.com>
2025-02-09 20:28:20 +0900

    게임 종료 연출 추가

--- Assets/Scripts/Contents/UI/UIEndGameObject.cs
+++ Assets/Scripts/Contents/UI/UIEndGameObject.cs
@@ -6,16 +6,68 @@ using UnityEngine;
 public class UIEndGameObject : MonoBehaviour
 {
     [SerializeField]
+    private TextMeshProUGUI resultText;
+
+    [SerializeField]
+    private TextMeshProUGUI waveText;
+
+    [SerializeField]
     private TextMeshProUGUI finalWaveText;
+
+    [SerializeField]
+    private TextMeshProUGUI compensationText;
+
+    [SerializeField]
+    private GameObject[] compensationOnActiveObjects;
+
+    [SerializeField]
+    private GameObject restartButton;
+
+    [SerializeField]
+    private GameObject[] endEffectActiveObjects;
 
     [SerializeField]
     private TextMeshProUGUI currencyText;
 
     private readonly string waveTextFormat = "{0}";
 
+    private Coroutine endGameEventCoroutine = null;
+
     private void OnEnable()
     {
         var currentWaveLevel = GameObject.FindWithTag(Tags.GameController).GetComponent<GameController>().SpawnSystem.CurrentWaveLevel;
         finalWaveText.text = string.Format(waveTextFormat, currentWaveLevel.ToString());
+
+        if (endGameEventCoroutine == null)
+            endGameEventCoroutine = StartCoroutine(CoEndGameEvent());
+    }
+
+    private IEnumerator CoEndGameEvent()
+    {
+        yield return new WaitForSecondsRealtime(0.5f);
+        
+        resultText.gameObject.SetActive(true);
+        yield return new WaitForSecondsRealtime(0.5f);
+
+        waveText.gameObject.SetActive(true);
+        yield return new WaitForSecondsRealtime(0.5f);
+        
+        finalWaveText.gameObject.SetActive(true);
+        yield return new WaitForSecondsRealtime(0.5f);
+
+        compensationText.gameObject.SetActive(true);
+
+        foreach (var compensationGameObject in compensationOnActiveObjects)
+        {
+            compensationGameObject.SetActive(true);
+        }
+
+        yield return new WaitForSecondsRealtime(0.5f);
+        restartButton.SetActive(true);
+
+        foreach (var endActiveGameObject in endEffectActiveObjects)
+        {
+            endActiveGameObject.SetActive(true);
+        }
     }
 }
---
+++ Assets/Scripts/Contents/UI/UIFadeInOut.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFadeInOut : MonoBehaviour
+{
+    [SerializeField]
+    private Image fadeInOutImage;
+
+    [SerializeField]
+    private bool isUnscale;
+
+    [SerializeField]
+    private bool isStartFadeOut;
+
+    [SerializeField]
+    private float fadeOutDuration;
+
+    private float currentTime;
+
+    private bool isFadeOut = true;
+
+    private Color currentColor;
+    private Color targetColor;
+
+    private void Awake()
+    {
+        currentColor = fadeInOutImage.color;
+        targetColor = currentColor;
+    }
+
+    private void Start()
+    {
+        if(isStartFadeOut)
+        {
+            targetColor.a = 1;
+            currentColor.a = 0;
+            fadeInOutImage.color = currentColor;
+        }
+        else
+        {
+            targetColor.a = 0;
+            currentColor.a = 1;
+            fadeInOutImage.color = currentColor;
+        }
+    }
+
+    private void Update()
+    {
+        if (isStartFadeOut)
+        {
+            FadeOut();
+        }
+        else
+        {
+            FadeIn();
+        }
+    }
+
+    private void FadeIn()
+    {
+        if(isUnscale)
+        {
+            currentTime += Time.unscaledDeltaTime;
+        }
+        else
+        {
+            currentTime += Time.deltaTime;
+        }
+
+
+        fadeInOutImage.color = Color.Lerp(currentColor, targetColor, currentTime / fadeOutDuration);
+
+        if (currentTime >= fadeOutDuration)
+        {
+            isStartFadeOut = true;
+            targetColor.a = 1;
+            currentColor.a = 0;
+            currentTime = 0f;
+        }
+    }
+    private void FadeOut()
+    {
+        if (isUnscale)
+        {
+            currentTime += Time.unscaledDeltaTime;
+        }
+        else
+        {
+            currentTime += Time.deltaTime;
+        }
+
+        fadeInOutImage.color = Color.Lerp(currentColor, targetColor, currentTime / fadeOutDuration);
+
+        if (currentTime >= fadeOutDuration)
+        {
+            isStartFadeOut = false;
+            targetColor.a = 0;
+            currentColor.a = 1;
+            currentTime = 0f;
+        }
+    }
+}
