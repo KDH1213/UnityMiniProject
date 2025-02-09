cc94e878be1014826b2c19c0b9eb708586db3f0e
Kim.Dong.Hyun <ehdgus0710@naver.com>
2025-02-09 13:22:59 +0900

    재화 획득 이펙트 오브젝트 풀 적용 및 UniTask 적용

--- Assets/Scripts/Contents/UI/AddCurrencyEffect.cs
+++ Assets/Scripts/Contents/UI/AddCurrencyEffect.cs
@@ -1,9 +1,16 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
+using System.Threading;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class AddCurrencyEffect : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI text;
+    public TextMeshProUGUI Text {  get { return text; } }
+
     [SerializeField]
     private Vector3 direction;
 
@@ -19,9 +26,31 @@ public class AddCurrencyEffect : MonoBehaviour
     [SerializeField]
     private Color targetColor;
 
-    private void Start()
+    private Vector3 startScale;
+
+    private UniTask currencyEffectUniTask;
+    private IObjectPool<AddCurrencyEffect> addCurrencyEffectPool;
+
+    private void Awake()
     {
-        StartCoroutine(CoEffect());
+        startScale = transform.localScale;
+    }
+
+    //private void Start()
+    //{
+    //    // StartCoroutine(CoEffect());
+    //    currencyEffectUniTask = CoCurrencyEffect();
+    //}
+
+    private void OnDisable()
+    {
+        text.color = Color.white;
+        transform.localScale = startScale;
+    }
+
+    public void StartEffect()
+    {
+        currencyEffectUniTask = CoCurrencyEffect();
     }
 
     private IEnumerator CoEffect()
@@ -29,9 +58,8 @@ public class AddCurrencyEffect : MonoBehaviour
         float currentTime = 0f;
 
         Vector3 position = transform.position;
-        Vector3 endPosition = transform.position + direction * distance;
+        Vector3 endPosition = position + direction * distance;
         Vector3 scale = transform.localScale;
-        var text = GetComponent<TextMeshProUGUI>();
         Color color = text.color;
 
         while (currentTime < time)
@@ -45,5 +73,42 @@ public class AddCurrencyEffect : MonoBehaviour
         }
 
         Destroy(gameObject);
+    }
+
+    private async UniTask CoCurrencyEffect()
+    {
+        float currentTime = 0f;
+        transform.localPosition = Vector3.zero;
+        Vector3 position = transform.localPosition;
+        Vector3 endPosition = position + direction * distance;
+        Vector3 scale = transform.localScale;
+        Color color = text.color;
+
+        while (currentTime < time)
+        {
+            currentTime += Time.deltaTime;
+            var ratio = currentTime / time;
+            transform.localPosition = Vector2.Lerp(position, endPosition, ratio);
+            transform.localScale = Vector2.Lerp(scale, targetScale, ratio);
+            text.color = Color.Lerp(color, targetColor, ratio);
+            await UniTask.Yield(PlayerLoopTiming.Update, cancellationToken:this.GetCancellationTokenOnDestroy());
+        }
+
+        DestroyUIDamageText();
+    }
+
+    public void SetPool(IObjectPool<AddCurrencyEffect> addCurrencyEffectPool)
+    {
+        this.addCurrencyEffectPool = addCurrencyEffectPool;
+    }
+
+    public IObjectPool<AddCurrencyEffect> GetObjectPool()
+    {
+        return addCurrencyEffectPool;
+    }
+
+    public void DestroyUIDamageText()
+    {
+        addCurrencyEffectPool.Release(this);
     }
 }
