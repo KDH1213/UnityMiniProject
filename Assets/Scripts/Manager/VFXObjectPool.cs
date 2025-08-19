a8d9c153ba38bd8281b61534ca7a8685b70b3cc7
김동현 <ehdgus0710@naver.com>
2025-08-19 17:41:04 +0900

    [chore] 테스트 환경 구성중

--- Assets/Scripts/Manager/VFXObjectPool.cs
+++ Assets/Scripts/Manager/VFXObjectPool.cs
@@ -18,7 +18,7 @@ public class VFXObjectPool : MonoBehaviour
         {
             if(!vfxObjectPoolTable.ContainsKey(vfxObject.ID))
             {
-                vfxObjectPoolTable.Add(vfxObject.ID, new ObjectPool<VFXObject>(OnCreateVFX, OnGetVFX, OnReleaseVFX, OnDestroyVFX, true, 2000));
+                vfxObjectPoolTable.Add(vfxObject.ID, new ObjectPool<VFXObject>(OnCreateVFX, OnGetVFX, OnReleaseVFX, OnDestroyVFX, true, 10000));
             }
         }
     }
